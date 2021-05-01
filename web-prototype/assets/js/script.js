65e1a98b8bccbf21cf39d19d692d47802f1f9c9c
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2021-05-02 03:46:52 +0530

    imrpoved code readability, ui enhancements

--- web-prototype/assets/js/script.js
+++ web-prototype/assets/js/script.js
@@ -1,3 +1,7 @@
+// $ is a function pointer for go.GraphObject.make()
+const $ = go.GraphObject.make;
+
+// get data asynchronously
 async function getData() {
   const response = await fetch("/web-prototype/assets/json/CPSC.json");
   const json = await response.json();
@@ -5,36 +9,77 @@ async function getData() {
   return json;
 }
 
+// get node and link arrays
 async function init() {
   const dataJson = await getData();
   const dataArray = dataJson.courses;
   const nodes = [];
-  const courses = [];
+  const links = [];
   dataArray.forEach((course) => {
     let v = { key: course.name };
-   nodes.push(v);
-   courses.push(course);
+    nodes.push(v);
+    links.push(course);
   });
   // console.log nodes);
   // console.log courses);
-  return { nodes, courses };
+  return { nodes, links: links };
 }
 
 async function createGraph() {
 
   const graphData = await init();
 
-  // $ is a function pointer for go.GraphObject.make()
-  const $ = go.GraphObject.make;
-
   // make diagram
-  const myDiagram = $(go.Diagram, "diagram-div", {
-    "undoManager.isEnabled": true,
-    layout: $(go.LayeredDigraphLayout), // alternate layout : go.ForceDirectedLayout
-  });
-  myDiagram.layout.direction = 90;
+  const myDiagram = createDiagram();
 
-  myDiagram.nodeTemplate = $(
+  // add nodes to new model
+  myDiagram.model = new go.GraphLinksModel(graphData.nodes);
+
+  // add links for edges
+  graphData.links.forEach((link) => {
+    const tempPrereqs = link.prereqs;
+    tempPrereqs.forEach((andCombo) => {
+      andCombo.forEach((orCombo) => {
+        if (orCombo.constructor === Array) {
+          return;
+        }
+        const currLink = {
+          from: orCombo,
+          to: link.name
+        }
+        // console.log(orCombo);
+        // console.log(link);
+        myDiagram.model.addLinkData(currLink);
+      });
+    });
+  });
+}
+
+// returns new diagram
+function createDiagram() {
+  return $(go.Diagram, "diagram-div", {
+    "undoManager.isEnabled": true,
+    initialAutoScale: go.Diagram.Uniform,
+    layout: createLayout(),
+    nodeTemplate: createNodeTemplate(),
+    linkTemplate: createLinkTemplate(),
+  });
+}
+
+// returns new layout
+function createLayout() {
+  const newLayout = new go.LayeredDigraphLayout();
+  newLayout.direction = 270;
+  newLayout.layerSpacing = 100;
+  newLayout.columnSpacing = 50;
+  newLayout.layeringOption = go.LayeredDigraphLayout.LayerLongestPathSink;
+  newLayout.aggressiveOption = go.LayeredDigraphLayout.AggressiveMore;
+  return newLayout;
+}
+
+// retuns new node template
+function createNodeTemplate() {
+  return nodeTemplate = $(
     go.Node,
     "Horizontal",
     { background: "#fc5185" },
@@ -47,24 +92,13 @@ async function createGraph() {
       new go.Binding("text", "key")
     )
   );
+}
 
-  myDiagram.model = new go.GraphLinksModel(graphData.nodes);
-
-  graphData.courses.forEach((course) => {
-    var tempPrereqs = course.prereqs;
-    tempPrereqs.forEach((andCombo) => {
-      andCombo.forEach((orCombo) => {
-        if(orCombo.constructor === Array){
-          return;
-        }
-        const link = {
-          from: orCombo,
-          to: course.name
-        }
-        // console.log(orCombo);
-        // console.log(link);
-        myDiagram.model.addLinkData(link);
-      });
-    });
-  });
+// returns new link template
+function createLinkTemplate() {
+  return LinkTemplate =
+    $(go.Link,
+      { routing: go.Link.AvoidsNodes, corner: 0 },
+      $(go.Shape),
+      $(go.Shape, { toArrow: "Standard" }));
 }

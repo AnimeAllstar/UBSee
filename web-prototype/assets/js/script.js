76a16d7af24042b60cab5541a7a3e244bbf6736c
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2021-05-02 06:39:18 +0530

    added color changing click event for nodes and links

--- web-prototype/assets/js/script.js
+++ web-prototype/assets/js/script.js
@@ -32,6 +32,11 @@ async function createGraph() {
   // make diagram
   const myDiagram = createDiagram();
 
+  // when the user clicks on the background of the Diagram, remove all highlighting
+  myDiagram.click = function (e) {
+    e.diagram.commit(function (d) { d.clearHighlighteds(); }, "no highlighteds");
+  };
+
   // add nodes to new model
   myDiagram.model = new go.GraphLinksModel(graphData.nodes);
 
@@ -80,17 +85,41 @@ function createLayout() {
 // retuns new node template
 function createNodeTemplate() {
   return nodeTemplate = $(
-    go.Node,
-    "Horizontal",
-    { background: "#fc5185" },
-    $(
-      go.TextBlock,
+    go.Node, "Auto",
+    {
+      click: function (e, node) {
+        const diagram = node.diagram;
+        // lock
+        diagram.startTransaction("highlight");
+        // remove any previous highlighting
+        diagram.clearHighlighteds();
+        // set Node.isHighlighted for all outgoing Links
+        node.findLinksOutOf().each(function (l) { l.isHighlighted = true; });
+        // set Node.isHighlighted for all outgoing Nodes
+        node.findNodesOutOf().each(function (n) { n.isHighlighted = true; });
+        // set Node.isHighlighted for all incoming Links
+        node.findLinksInto().each(function (l) { l.isHighlighted = true; });
+        // set Node.isHighlighted for all incoming Nodes
+        node.findNodesInto().each(function (n) { n.isHighlighted = true; });
+        // unlock
+        diagram.commitTransaction("highlight");
+      }
+    },
+    $(go.Shape, "Rectangle",
+      { strokeWidth: 2, stroke: null, fill: "#FFF" },
+      new go.Binding("fill", "color"),
+      // the Shape.stroke color depends on whether Node.isHighlighted is true
+      new go.Binding("stroke", "isHighlighted", function (h) { return h ? "#FC5185" : "#000"; }).ofObject(),
+      new go.Binding("fill", "isHighlighted", function (h) { return h ? "#FC5185" : "#FFF"; }).ofObject(),
+      new go.Binding("stroke", "isSelected", function (h) { return h ? "#1e90ff" : "#000"; }).ofObject(),
+      new go.Binding("fill", "isSelected", function (h) { return h ? "#1e90ff" : "#FFF"; }).ofObject()),
+    $(go.TextBlock,
       "course id", // default text
       // text config, padding
-      { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
-      // TextBlock.text is bound to Node.data.key
-      new go.Binding("text", "key")
-    )
+      { margin: 12, stroke: "#000", font: "bold 16px sans-serif" },
+      new go.Binding("stroke", "isHighlighted", function (h) { return h ? "#FFF" : "#000"; }).ofObject(),
+      new go.Binding("stroke", "isSelected", function (h) { return h ? "#FFF" : "#000"; }).ofObject(),
+      new go.Binding("text", "key"))
   );
 }
 
@@ -99,6 +128,17 @@ function createLinkTemplate() {
   return LinkTemplate =
     $(go.Link,
       { routing: go.Link.AvoidsNodes, corner: 0 },
-      $(go.Shape),
-      $(go.Shape, { toArrow: "Standard" }));
+      $(go.Shape,
+        // the Shape.stroke color depends on whether Link.isHighlighted is true
+        new go.Binding("stroke", "isHighlighted", function (h) { return h ? "#FC5185" : "black"; })
+          .ofObject(),
+        // the Shape.strokeWidth depends on whether Link.isHighlighted is true
+        new go.Binding("strokeWidth", "isHighlighted", function (h) { return h ? 3 : 1; })
+          .ofObject()),
+      $(go.Shape,
+        { toArrow: "Standard", strokeWidth: 0 },
+        // the Shape.fill color depends on whether Link.isHighlighted is true
+        new go.Binding("fill", "isHighlighted", function (h) { return h ? "#FC5185" : "black"; })
+          .ofObject())
+    );
 }

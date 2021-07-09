85c0f7d494f547ae4487dd774c011bbdfe7a5e23
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2021-07-10 01:26:46 +0530

    added controller for 404

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const favicon = require('serve-favicon');
 const compression = require('compression');
 const helmet = require('helmet');
 const nunjucks = require('nunjucks');
-
 const path = require('path');
+
 global.appRoot = path.resolve(__dirname);
 
 const appRoutes = require('./routes/routes.js');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -33,14 +34,7 @@ nunjucks.configure('views', {
 app.use(appRoutes);
 
 // request reaches here if none of the routes in appRoutes is matched
-app.use((req, res) => {
-  res.status(404).render(path.join(global.appRoot, 'views', '404.html'), {
-    title: "404 - Page Not Found",
-    description: '404 - Page Not Found',
-    robots: 'noindex, follow',
-    keywords: 'UBSee, UBC'
-  });
-});
+app.use(errorController.render404);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('listening on ' + (process.env.PORT ? `port ${process.env.PORT}` : 'http://localhost:3000/'));

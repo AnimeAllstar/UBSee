ba7c16b031b1f85507f2fa0c50f73df0c4b941c5
ASad Dhorajiwala <ashcarter.awesome@gmail.com>
2021-08-08 23:02:54 +0530

    index.css now gets loaded before other components

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import './index.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import NotFound from './components/NotFound';
 import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
